16f7a8b8ffdae8091e779c290614135a910b420e
Matthias Marschall <mm@agileweboperations.com>
2013-07-27 17:32:25 +0200

    all nginx cookbooks

--- cookbooks/my_cookbook/metadata.rb
+++
@@ -1,7 +0,0 @@
-name              "ntp"
-maintainer        "Matthias Marschall"
-maintainer_email  "mm@agileweboperations.com"
-license           "Apache 2.0"
-version           "0.1.0"
-
-depends "snmp"
